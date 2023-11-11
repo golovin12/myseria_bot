26f79f62cdccad530e3b0eb756d66148a84fa402
golovin12 <golovin.denis@lenta.ru>
2023-11-11 20:14:51 +0500

    test_admin_controller

--- bots/admin_bot/controller.py
+++ bots/admin_bot/controller.py
@@ -6,8 +6,20 @@ from database.models import Admin, SerialSite
 class AdminController:
     def __init__(self, user_id: int):
         self.user_id = user_id
-        self._admin = None
-        self._access_denied_msg = "Отказано в доступе"
+
+    async def create_admin(self, new_admin_id: int) -> str:
+        await Admin(new_admin_id, is_admin=True).save()
+        return f"Юзер с id {new_admin_id} теперь является администратором"
+
+    async def delete_admin(self, admin_id: int) -> str:
+        if admin_id == self.user_id:
+            return "Вы не можете убрать с себя роль администратора"
+        await Admin(admin_id, is_admin=False).save()
+        return f"Юзер с id {admin_id} больше не является администратором"
+
+    async def get_all_admins(self) -> str:
+        admins = await Admin.get_admins_id()
+        return "\n".join(admins)
 
     async def force_update_my_seria_url(self, new_url: str) -> str:
         """Обновляем ссылку на сайт MySeria"""
@@ -31,25 +43,3 @@ class AdminController:
         if await serial_site.save():
             return f'Адрес успешно обновлён на: {serial_site.url}'
         return 'Не удалось обновить адрес'
-
-    async def get_admin(self) -> Admin:
-        if self._admin is None:
-            self._admin = await Admin.get_object(self.user_id)
-        return self._admin
-
-    async def create_admin(self, new_admin_id: int) -> str:
-        admin = await self.get_admin()
-        admin.is_admin = True
-        await admin.save()
-        return f"Юзер с id {new_admin_id} теперь является администратором"
-
-    async def delete_admin(self, admin_id: int) -> str:
-        admin = await self.get_admin()
-        if admin_id == admin.user_id:
-            return "Вы не можете убрать с себя роль администратора"
-        await Admin(admin_id, is_admin=False).save()
-        return f"Юзер с id {admin_id} больше не является администратором"
-
-    async def get_all_admins(self) -> str:
-        admins = await Admin.get_admins_id()
-        return "\n".join(admins)
---
+++ tests/test_admin_controller.py
@@ -0,0 +1,76 @@
+import asyncio
+import warnings
+from unittest.mock import patch
+
+from bots.admin_bot.controller import AdminController
+from consts import MySeria, Zetflix
+from database import ObjectNotFoundError
+from database.models import Admin, SerialSite
+from .base_test_case import DBTestCase
+
+
+class AdminControllerTest(DBTestCase):
+    def test_create_admin(self):
+        warnings.simplefilter("ignore", ResourceWarning)
+        admin_id = 2
+        admin = asyncio.run(Admin.get_object(admin_id))
+        self.assertFalse(admin.is_admin)
+        asyncio.run(AdminController(1).create_admin(admin_id))
+        admin = asyncio.run(Admin.get_object(admin_id))
+        self.assertTrue(admin.is_admin)
+
+    def test_delete_admin(self):
+        warnings.simplefilter("ignore", ResourceWarning)
+        admin_id = 2
+        # попытка удалить несуществующего админа
+        asyncio.run(AdminController(3).delete_admin(admin_id))
+        # создание админа
+        asyncio.run(Admin(admin_id, True).save())
+        admin = asyncio.run(Admin.get_object(admin_id))
+        self.assertTrue(admin.is_admin)
+        # попытка забрать роль администратора у себя
+        asyncio.run(AdminController(admin_id).delete_admin(admin_id))
+        admin = asyncio.run(Admin.get_object(admin_id))
+        self.assertTrue(admin.is_admin)
+        # забираем роль администратора у другого
+        asyncio.run(AdminController(3).delete_admin(admin_id))
+        admin = asyncio.run(Admin.get_object(admin_id))
+        self.assertFalse(admin.is_admin)
+
+    def test_get_all_admins(self):
+        warnings.simplefilter("ignore", ResourceWarning)
+        self.assertEqual(asyncio.run(AdminController(3).get_all_admins()), '')
+        asyncio.run(Admin(2, True).save())
+        asyncio.run(Admin(3, True).save())
+        self.assertEqual(set(asyncio.run(AdminController(3).get_all_admins()).split('\n')), {'2', '3'})
+
+    def test_update_my_seria_url(self):
+        warnings.simplefilter("ignore", ResourceWarning)
+        with patch('bots.admin_bot.controller.url_is_active') as mock_url_is_active:
+            mock_url_is_active.return_value = True
+            self.assertRaises(ObjectNotFoundError, asyncio.run, SerialSite.get_object(MySeria.KEY))
+            # Попытка сохранить невалидный url
+            asyncio.run(AdminController(3).force_update_my_seria_url('new_url'))
+            self.assertRaises(ObjectNotFoundError, asyncio.run, SerialSite.get_object(MySeria.KEY))
+            # Попытка сохранить неактивный url
+            mock_url_is_active.return_value = False
+            new_url = 'https://test_url.com'
+            asyncio.run(AdminController(3).force_update_my_seria_url(new_url))
+            self.assertRaises(ObjectNotFoundError, asyncio.run, SerialSite.get_object(MySeria.KEY))
+            # Сохранение нового url
+            mock_url_is_active.return_value = True
+            asyncio.run(AdminController(3).force_update_my_seria_url(new_url))
+            my_seria_site = asyncio.run(SerialSite.get_object(MySeria.KEY))
+            self.assertEqual(my_seria_site.url, new_url)
+
+    def test_update_zetflix_url(self):
+        warnings.simplefilter("ignore", ResourceWarning)
+        self.assertRaises(ObjectNotFoundError, asyncio.run, SerialSite.get_object(Zetflix.KEY))
+        # Попытка сохранить невалидный url
+        asyncio.run(AdminController(3).force_update_zetflix_url('new_url'))
+        self.assertRaises(ObjectNotFoundError, asyncio.run, SerialSite.get_object(Zetflix.KEY))
+        # Сохранение нового url
+        new_url = 'https://test_url.com'
+        asyncio.run(AdminController(3).force_update_zetflix_url(new_url))
+        my_seria_site = asyncio.run(SerialSite.get_object(Zetflix.KEY))
+        self.assertEqual(my_seria_site.url, new_url)
