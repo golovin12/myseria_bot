84f050657d3f11a598624e5c541b34a479a34135
golovin12 <golovin.denis@lenta.ru>
2023-11-18 06:11:45 +0500

    updated README.md and added future schema

--- config/settings_production.py
+++ config/settings_production.py
@@ -10,7 +10,7 @@ from setup.logging_config import configure_logging
 
 class ProductionSettings:
     def __init__(self):
-        self.ENV_NAME = 'PROD'
+        self.ENV_NAME = 'PRODUCTION'
         self.REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
         self.RABBITMQ_HOST = os.getenv('RABBITMQ_HOST', 'localhost')
         # хранилище для database.models
--- management/logging_handlers.py
+++ management/logging_handlers.py
@@ -6,4 +6,6 @@ from bots.admin_bot.tasks import admin_bot_send_message
 
 class AdminHandler(logging.Handler):
     def emit(self, record: logging.LogRecord):
-        admin_bot_send_message.send(settings.ADMIN_ID, self.format(record))
+        admin_id = settings.ADMIN_ID
+        if admin_id:
+            admin_bot_send_message.send(admin_id, self.format(record))
